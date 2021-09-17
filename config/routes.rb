7cf297f17e452677c5bb5130be13a59711bb9cb1
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-17 08:11:49 +0000

    [Update]コンフリクト解消

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,12 @@ Rails.application.routes.draw do
   registrations: 'admin/registrations'
 }
  root to: 'admin/homes#top'
+  
+  namespace :admin do
+    resources :members, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+  end
+  
   devise_for :members
-  namespace :admin do
-    get 'products/index'
-    get 'products/show'
-    get 'products/edit'
-    get 'products/update'
-    get 'products/new'
-    get 'products/create'
-    get 'products/destroy'
-  end
-  namespace :admin do
-    get 'order_products/update'
-  end
-  namespace :admin do
-    get 'orders/show'
-    get 'orders/update'
-  end
-  namespace :admin do
-    get 'members/index'
-    get 'members/show'
-    get 'members/edit'
-    get 'members/update'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
