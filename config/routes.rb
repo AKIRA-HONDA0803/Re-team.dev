3203b29f4022027e715f9714c954ada27a8f4f51
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-18 05:56:02 +0000

    [Add]会員側routes作成

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,28 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     root to: 'homes#top'
   end
+
+  devise_for :members, controllers: {
+  sessions:      'public/sessions',
+  passwords:     'public/passwords',
+  registrations: 'public/registrations'
+}
+  get 'members/unsubscribe' => 'public/members#unsubscribe'
+  patch 'members/withdraw' => 'public/members#withdraw'
+  resources :members, only: [:show, :edit, :update]
+
+  post 'orders/confirm' => 'public/orders#confirm'
+  get 'orders/complete' => 'public/orders#complete'
+  resources :orders, only: [:index, :show, :new, :create]
+
+  delete 'cart_products/destroy_all' => 'public/cart_products#destroy_all'
+  resources :cart_products, only: [:index, :create, :destroy, :update]
+
+  resources :addresses, except: [:show, :new]
+
+  resources :products, only: [:index, :show]
+
+  get 'about' => 'public/homes#about'
+  root to: 'public/homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
