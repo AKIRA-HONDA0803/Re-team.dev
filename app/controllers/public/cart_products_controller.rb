39693f126ec870442eaa4a028e9713883bfc4d16
EC2 Default User <ec2-user@ip-172-31-72-225.ec2.internal>
2021-09-18 10:29:17 +0000

    pullでのエラーの為

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,18 +1,46 @@
 class Public::CartProductsController < ApplicationController
 
  def index
+ 
+  @cart_products = CartProduct.where(member_id: current_member.id)
  end
 
- def create
- end
+  def create
+    @cart_product = CartProduct.new(cart_product_params)
+    @cart_products = CartProduct.where(customer_id: current_customer.id)
+    @cart_products.each do |cart_product|
+      if cart_product.product_id == @cart_product.product_id
+        new_total_price = cart_product.total_price + @cart_product.total_price
+        cart_product.update_attribute(:total_price, new_total_price)
+        @cart_product.delete
+      end
+    end
+    @cart_product.save
+    redirect_to cart_products_path
+  end
 
  def destroy
+　cart_product = CartProduct.find(params[:id])
+  cart_product.destroy
+  redirect_to cart_products_path
  end
 
  def update
+  @cart_product = CartProduct.find(params[:id])
+   if @cart_product.update(cart_product_params)
+   redirect_to cart_products_path
+   end
  end
 
- def destroy_all
- end
+  def destroy_all
+    cart_products = CartProduct.where(member_id: current_member.id)
+    cart_products.destroy_all
+    redirect_to cart_products_path
+  end
+  
+    private
+  def cart_product_params
+    params.require(:cart_product).permit(:product_id, :member_id, :total_price)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
     resources :genres, only: [:index, :create, :edit, :update]
     root to: 'homes#top'
   end
-    
+    delete '/cart_products/destroy_all', to: 'public/cart_products#destroy_all'
+    resources :cart_products, module: :public, only: [:index, :update, :destroy]
+    post '/cart_products', to: 'public/cart_products#create',  as: 'create_cart_products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
