fdd021c3e04317149e11c4da5adef46064389054
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-22 06:08:08 +0000

    hideaki

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,7 +14,7 @@ class Admin::ProductsController < ApplicationController
  def update
   @product = Product.find(params[:id])
   if @product.update(product_params)
-     redirect_to admin_products_path
+     redirect_to admin_product_path
   else
    flash[:error] = "入力してください"
       redirect_to 'edit'
@@ -28,7 +28,7 @@ class Admin::ProductsController < ApplicationController
  def create
   @product = Product.new(product_params)
   if @product.save
-      redirect_to admin_products_path
+      redirect_to admin_product_path(@product)
   else
       flash[:error] = "入力してください"
       redirect_to new_admin_product_path
