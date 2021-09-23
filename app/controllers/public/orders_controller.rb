97f41608c6568326da0385dc3552a247244ed545
KRofLife <05410kira@gmail.com>
2021-09-23 06:05:30 +0000

    orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,7 +2,8 @@ class Public::OrdersController < ApplicationController
  def new
   @order = Order.new
   @addresses = current_member.addresses
-  @address_new = Address.new
+  @address = Address.new
+  # @new_address = Address.new
   # @addresses = Address.where(member: current_member)
  end
 
@@ -14,9 +15,21 @@ class Public::OrdersController < ApplicationController
  def create
   # @order = current_member.orders.new(order_params)
   @order = Order.new(order_params)
-  @order.save
+   if @order.save
   flash[:notice] = "ご注文を確認しました。"
+  redirect_to orders_complete_path
+# ラジオボタンで選択されたお届け先によって条件分岐
+
+
+            # @order.postal_code = params[:order][:postal_code]
+            # @order.address = params[:order][:address]
+
+            # @order.name = params[:order][:name]
+   end
  end
+
+
+
 
  def confirm
   @order = Order.new(order_params)
@@ -26,24 +39,25 @@ class Public::OrdersController < ApplicationController
   @order.shipping_fee= 800
   @order.payment_method = params[:order][:payment_method]
 
-  #「お届け先」は、①ご自身の住所②登録済住所③新しいお届け先のどれかをラジオボタンで選択する分岐
-
- if params[:order] == "0"
+        if params[:order][:select_address] == "0"
             @order.postal_code = current_member.postal_code
             @order.address = current_member.address
             @order.name = current_member.last_name + current_member.first_name
- elsif params[:order] == "1"
-            #f.selectのaddress_boxの中の/:id の番号を取得する
-            @address = Address.find(params[:order])
+        elsif params[:order][:select_address] == "1"
+            @address = Address.find(params[:order][:address_selection])
             @order.postal_code = @address.postal_code
             @order.address = @address.address
             @order.name = @address.name
-
- elsif params[:order] == "2"
+        elsif params[:order][:select_address] == "2"
             @order.postal_code = params[:order][:postal_code]
             @order.address = params[:order][:address]
             @order.name = params[:order][:name]
- end
+        end
+
+
+  #「お届け先」は、①ご自身の住所②登録済住所③新しいお届け先のどれかをラジオボタンで選択する分岐
+
+
  end
 
  def complete
@@ -55,7 +69,7 @@ class Public::OrdersController < ApplicationController
 
  private
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_price)
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method)
   end
   def address_params
     params.require(:order).permit(:postal_code, :address, :name)
