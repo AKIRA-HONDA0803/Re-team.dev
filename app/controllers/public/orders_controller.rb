1269a56d663fbc38415635f55752741b408a9aa7
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-23 08:07:58 +0000

    hideaki

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,6 +10,7 @@ class Public::OrdersController < ApplicationController
  def show
   @order = Order.find(params[:id])
   @order_status = @order.order_status
+  @orders = @order.order_products
  end
 
  def create
@@ -18,7 +19,18 @@ class Public::OrdersController < ApplicationController
    if @order.save
   flash[:notice] = "ご注文を確認しました。"
   redirect_to orders_complete_path
-  current_member.cart_products.destroy_all
+
+  cart_products = current_member.cart_products
+   cart_products.each do |item|
+       order_product = OrderProduct.new
+       order_product.order_id = @order.id
+       order_product.product_id = item.product.id
+       order_product.quantity = item.quantity
+       order_product.total_price = item.quantity*item.product.price
+       order_product.making_status = 0
+       order_product.save
+      end
+       current_member.cart_products.destroy_all
 
    end
  end
