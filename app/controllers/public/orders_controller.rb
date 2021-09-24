5c697ad78b660800523454e05fd5c546a2356bf4
EC2 Default User <ec2-user@ip-172-31-72-225.ec2.internal>
2021-09-24 06:35:42 +0000

    注文ステータスの修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,67 +1,26 @@
 class Public::OrdersController < ApplicationController
  def new
   @order = Order.new
-  @addresses = current_member.addresses
-  @address = Address.new
-  # @new_address = Address.new
-  # @addresses = Address.where(member: current_member)
+  @member = current_member
+  @addresses = Address.where(member: current_member)
  end
 
  def show
   @order = Order.find(params[:id])
-  @order_status = @order.order_status
-  @orders = @order.order_products
+  @order_details = @order.order_details
  end
 
  def create
-  @order = Order.new(order_params)
-  @order.member = current_member
-  @cart_products = current_member.cart_products
-
-   if @order.save
+  @order = current_member.orders.new(order_params)
+  @order.save
   flash[:notice] = "ご注文を確認しました。"
-  redirect_to orders_complete_path
-
-
-
-  cart_products = current_member.cart_products
-   cart_products.each do |item|
-       order_product = OrderProduct.new
-       order_product.order_id = @order.id
-       order_product.product_id = item.product.id
-       order_product.quantity = item.quantity
-       order_product.total_price = item.quantity*item.product.price
-       order_product.making_status = 0
-       order_product.save
-      end
-       current_member.cart_products.destroy_all
-   end
  end
 
  def confirm
-  @order = Order.new(order_params)
+  @order = Order.new
   @member = current_member
+  @addresses = Address.where(member_id: current_member.id)
   @cart_products = current_member.cart_products
-  @order.shipping_fee= 800
-  @order.payment_method = params[:order][:payment_method]
-
-        if params[:order][:select_address] == "0"
-            @order.postal_code = current_member.postal_code
-            @order.address = current_member.address
-            @order.name = current_member.last_name + current_member.first_name
-        elsif params[:order][:select_address] == "1"
-            address = Address.find(params[:order][:address_selection])
-            @order.postal_code = address.postal_code
-            @order.address = address.address
-            @order.name = address.name
-        elsif params[:order][:select_address] == "2"
-            @order.postal_code = params[:order][:postal_code]
-            @order.address = params[:order][:address]
-            @order.name = params[:order][:name]
-        end
- end
-
- def complete
  end
 
  def index
@@ -70,10 +29,9 @@ class Public::OrdersController < ApplicationController
 
  private
   def order_params
-    params.require(:order).permit(:postal_code, :address, :name, :payment_method,:shipping_fee,:bill,:order_status )
+    params.require(:order).permit(:postal_code, :address, :name, :payment_method, :total_price)
   end
   def address_params
     params.require(:order).permit(:postal_code, :address, :name)
   end
-
 end
