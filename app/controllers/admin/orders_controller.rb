74ced395036e354692a015fbee15719e23e25c1c
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-25 02:48:33 +0000

    hideaki

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,8 +9,15 @@ class Admin::OrdersController < ApplicationController
  def update
   @order = Order.find(params[:id])
   if @order.update(order_params)
-     flash[:success] = "注文ステータスを変更しました"
-     redirect_to admin_order_path(@order)
+   flash[:success] = "注文ステータスを変更しました"
+   if @order.order_status == "入金確認"
+    order_products = @order.order_products
+    order_products.each do |order_product|
+     order_product.making_status = "製作待ち"
+     order_product.save
+    end
+   end
+   redirect_to admin_order_path(@order)
   else
      render "show"
   end
