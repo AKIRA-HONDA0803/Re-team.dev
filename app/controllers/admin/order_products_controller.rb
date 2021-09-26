e136ebde4182f2f2fca3e71cddfdbf6ce6fa8cf3
horkar667 <jmmpp413@yahoo.co.jp>
2021-09-26 05:54:43 +0000

    hideaki

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -4,7 +4,16 @@ class Admin::OrderProductsController < ApplicationController
   def update
    @order_product = OrderProduct.find(params[:id])
    @order_product.update(order_product_params)
-   flash[:success] = "製作ステータスを変更しました"
+    order = @order_product.order
+    @order_products = order.order_products
+    ordered_items_completed = @order_products.where(making_status: "製作完了" )
+    if @order_product.making_status == "製作中"
+      @order_product.order.order_status = "製作中"
+      @order_product.order.save
+    elsif @order_products.count == ordered_items_completed.count
+      @order_product.order.order_status = "発送準備中"
+      @order_product.order.save
+    end
    redirect_to admin_order_path(@order_product.order)
   end
 
